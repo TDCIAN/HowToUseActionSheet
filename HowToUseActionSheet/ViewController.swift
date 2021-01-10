f9c444c57c7252904974800746fbf054cbde4e13
Jeongmin <tdcian71@gmail.com>
2021-01-10 23:06:33 +0900

    액션시트 메소드 추가

--- HowToUseActionSheet/ViewController.swift
+++ HowToUseActionSheet/ViewController.swift
@@ -17,6 +17,34 @@ class ViewController: UIViewController {
     }
     @IBAction func UseActionSheet(_ sender: UIButton) {
         print("버튼 성능 확실하구만")
+        showActionSheet()
+    }
+    
+    func showActionSheet() {
+        let actionSheet = UIAlertController(title: "액숀시트 이름", message: "나는 액션시트입니다", preferredStyle: .actionSheet)
+        
+        let first = UIAlertAction(title: "첫번째 선택지", style: .default) { action in
+            print("첫번째 선택지를 선택했습니다.")
+        }
+        
+        let second = UIAlertAction(title: "두번째 선택지", style: .default) { action in
+            print("두번째 선택지를 선택했습니다.")
+        }
+        
+        let third = UIAlertAction(title: "세번째 선택지", style: .default) { action in
+            print("세번째 선택지를 선택했습니다.")
+        }
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel) { action in
+            print("취소를 선택했습니다.")
+        }
+        
+        actionSheet.addAction(first)
+        actionSheet.addAction(second)
+        actionSheet.addAction(third)
+        actionSheet.addAction(cancel)
+        
+        present(actionSheet, animated: true, completion: nil)
     }
 }
 
